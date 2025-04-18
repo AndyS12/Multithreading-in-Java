584c8095d67a45546acd5ee2825460f73d46e70e
Anand Singh <anandsingh372@gmail.com>
2025-04-19 00:59:04 +0530

    EasyLevel: Print 1 to N using M threads in sequence

---
+++ Problems/src/EasyLevel/PrintUsingNThreads/Main.java
@@ -0,0 +1,12 @@
+package EasyLevel.PrintUsingNThreads;
+
+public class Main {
+    public static void main(String[] args) {
+        SharedNumberClass sharedNumberClass = new SharedNumberClass(16);
+        int totalThreads = 3;
+        for(int i = 0; i < totalThreads; i++){
+            Thread thread = new Thread(new Printer(sharedNumberClass, totalThreads, i), "Thread - " + i);
+            thread.start();
+        }
+    }
+}
---
+++ Problems/src/EasyLevel/PrintUsingNThreads/SharedNumberClass.java
@@ -0,0 +1,15 @@
+package EasyLevel.PrintUsingNThreads;
+
+public class SharedNumberClass {
+    int MAX_LIMIT;
+    int currentNumber = 1;
+
+    public SharedNumberClass(int MAX_LIMIT) {
+        this.MAX_LIMIT = MAX_LIMIT;
+    }
+
+    public void increment() {
+        System.out.printf("%d printed by %s\n", currentNumber, Thread.currentThread().getName());
+        currentNumber++;
+    }
+}
