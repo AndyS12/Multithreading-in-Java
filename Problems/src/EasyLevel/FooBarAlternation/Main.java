237b603c4971509f4f0ebba52268c350dc24c47c
Anand Singh <anandsingh372@gmail.com>
2025-05-06 15:38:05 +0530

    EasyLevel: Foo Bar printing using semaphores

--- Problems/src/EasyLevel/FooBarAlternation/Main.java
+++ Problems/src/EasyLevel/FooBarAlternation/Main.java
@@ -1,11 +1,33 @@
 package EasyLevel.FooBarAlternation;
 
+import java.util.concurrent.Semaphore;
+
 public class Main {
-    public static void main(String []args){
-        SharedClass sharedClass = new SharedClass("foo");
-        for(int i = 0; i<2; i++){
-            Thread thread = new Thread(new FooBarPrinter(sharedClass, i));
-            thread.start();
-        }
+    public static void main(String[] args) {
+        Semaphore pingSemaphore = new Semaphore(1);
+        Semaphore pongSemaphore = new Semaphore(0);
+
+        new Thread(() -> {
+            while (true) {
+                try {
+                    pingSemaphore.acquire();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("Foo printed by " + Thread.currentThread().getName());
+                pongSemaphore.release();
+            }
+        }).start();
+        new Thread(() -> {
+            while (true) {
+                try {
+                    pongSemaphore.acquire();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("Bar printed by " + Thread.currentThread().getName());
+                pingSemaphore.release();
+            }
+        }).start();
     }
 }
